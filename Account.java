750cc3d00d8e24f34a5ad5cad9f677015d981c13
shams-rasulzade <95118576+shams-rasulzade@users.noreply.github.com>
2022-05-20 22:20:50 +0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,26 +1,76 @@
-public class Account 
-{
-    String name;
-    String password;
-    double balance;
+public class Account {
+	String name;
+	String password;
+	double balance_usd;
+	double balance_azn;
 
-	void setBalance(double accBal) 
-    {
-		balance = accBal;
+	public Account(String name, String password, double balance_usd, double balance_azn) {
+		this.name = name;
+		this.password = password;
+		this.balance_usd = balance_usd;
+		this.balance_azn = balance_azn;
 	}
 
-    double getBalance() 
-    {
-		return balance;
+
+	// *** Name and Password
+
+	String getName() {
+		return name;
 	}
 
-	void deposit(double dep) 
-    {
-		balance += dep;
+	String getPassword() {
+		return password;
 	}
 
-	void withdraw(double wit) 
-    {
-	    balance -= wit;
+	// *** USD ***
+
+	double getBalanceUSD() {
+		return balance_usd;
+	}
+
+	void depositUSD(double dep) {
+		if (dep < 0) {
+			System.out.println("This is not proper amount.\n");
+		} else {
+			balance_usd += dep;
+			System.out.println("Money has been successfully deposited!");
+			System.out.println("\nYour balance is now: " + this.balance_usd + "$\n");
+		}
+	}
+
+	void withdrawUSD(double wit) {
+		if (wit > balance_usd) {
+			System.out.println("Your balance is not enough.\n");
+		} else {
+			balance_usd -= wit;
+			System.out.println("\nYour balance is now: " + this.balance_usd + "$\n");
+			System.out.println("Please take your money\n");
+		}
+	}
+
+	// *** AZN ***
+
+	double getBalanceAZN() {
+		return balance_azn;
+	}
+
+	void depositAZN(double dep) {
+		if (dep < 0) {
+			System.out.println("This is not proper amount.\n");
+		} else {
+			balance_azn += dep;
+			System.out.println("Money has been successfully deposited!");
+			System.out.println("\nYour balance is now: " + this.balance_azn + " AZN\n");
+		}
+	}
+
+	void withdrawAZN(double wit) {
+		if (wit > balance_azn) {
+			System.out.println("Your balance is not enough.\n");
+		} else {
+			balance_azn -= wit;
+			System.out.println("\nYour balance is now: " + this.balance_azn + " AZN\n");
+			System.out.println("Please take your money\n");
+		}
 	}
 }
